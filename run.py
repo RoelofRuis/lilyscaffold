cb581f8bfd83fe01b4faf4d44f9ab1c028744ad5
Roelof Ruis <roelof@branchonline.nl>
2016-10-02 16:12:00 +0200

    moved data to run

--- run.py
+++ run.py
@@ -1,15 +1,36 @@
 from templating.factory import *
 
-# Testing the system
 factory = LilypondFactory()
 
-factory.createTemplate('main', 'out.ly', {
+# Options file
+factory.createTemplate('options', 'options.ly', {
     'version' : '2.18.2',
-    'title' : 'Title',
-    'subtitle' : 'Subtitle',
-    'composer' : 'Roelof Ruis',
-    'meter' : '',
-    'piece' : '',
+    'time'    : '4/4',
+    'tempo'   : '4 = 120',
+    'key'     : 'c \minor',
+}).run()
+
+factory.createTemplate('instrument', 'violin.ly', {
+    'version'        : '2.18.2',
+    'instrument'     : 'violin',
+    'clef'           : 'treble',
+    'instrumentName' : 'violin',
+}).run()
+
+factory.createTemplate('instrument', 'guitar.ly', {
+    'version'        : '2.18.2',
+    'instrument'     : 'guitar',
+    'clef'           : 'treble',
+    'instrumentName' : 'guitar',
+}).run()
+
+factory.createTemplate('main', 'main.ly', {
+    'version'     : '2.18.2',
+    'title'       : 'Title',
+    'subtitle'    : 'Subtitle',
+    'composer'    : 'Roelof Ruis',
+    'meter'       : '',
+    'piece'       : '',
     'instruments' : [
         {
             'instrumentName' : 'violin',
