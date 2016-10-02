69913bfdfeb8401101c45039135363fbea0331bf
Roelof Ruis <roelof@branchonline.nl>
2016-10-02 17:06:44 +0200

    Trying out watchdog for automatic building

---
+++ watch.py
@@ -0,0 +1,23 @@
+import sys
+import time
+import logging
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+class TestHandler(FileSystemEventHandler):
+    def on_any_event(self, event):
+        print event
+
+if __name__ == "__main__":
+    path = 'out/.'
+    event_handler = TestHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
+                
