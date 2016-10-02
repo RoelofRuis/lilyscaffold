acba789e4581b07cd35507d4a28aa44b1e7524ac
Roelof Ruis <roelof@branchonline.nl>
2016-10-03 00:35:18 +0200

    moving creating code to factory completely and removing duplication from definition

--- run.py
+++ run.py
@@ -2,41 +2,29 @@ from templating.factory import *
 
 factory = LilypondFactory()
 
-# Options file
-factory.createTemplate('options', 'options.ly', {
-    'version' : '2.18.2',
-    'time'    : '4/4',
-    'tempo'   : '4 = 120',
-    'key'     : 'c \minor',
-}).run()
-
-factory.createTemplate('instrument', 'violin.ly', {
-    'version'        : '2.18.2',
-    'instrument'     : 'violin',
-    'clef'           : 'treble',
-    'instrumentName' : 'violin',
-}).run()
-
-factory.createTemplate('instrument', 'guitar.ly', {
-    'version'        : '2.18.2',
-    'instrument'     : 'guitar',
-    'clef'           : 'treble',
-    'instrumentName' : 'guitar',
-}).run()
-
-factory.createTemplate('main', 'main.ly', {
-    'version'     : '2.18.2',
+general = {
     'title'       : 'Title',
     'subtitle'    : 'Subtitle',
     'composer'    : 'Roelof Ruis',
-    'meter'       : '',
-    'piece'       : '',
-    'instruments' : [
-        {
-            'instrumentName' : 'violin',
-        },
-        {
-            'instrumentName' : 'guitar',
-        }
-    ]
-}).run()
+}
+
+options = {
+    'time'    : '4/4',
+    'tempo'   : '4 = 120',
+    'key'     : 'c \minor',
+}
+
+instruments = [
+    {
+        'instrument'     : 'violin',
+        'clef'           : 'treble',
+        'instrumentName' : 'violin',
+    },
+    {
+        'instrument'     : 'guitar',
+        'clef'           : 'treble',
+        'instrumentName' : 'guitar',
+    }
+]
+
+factory.createProject(general, options, instruments)
--- templating/factory.py
+++ templating/factory.py
@@ -3,14 +3,24 @@ from io import *
 from fillers import *
 
 class LilypondFactory():
+    def createProject(self, general, options, instruments):
+        self.versionFiller = VersionFiller()
+        self.cleanupFiller = CleanupFiller()
+        self.createTemplate('options', 'options.ly', options).run()
+        for instrument in instruments:
+            self.createTemplate('instrument', instrument['instrumentName'] + '.ly', instrument).run()
+        mainReplacements = general.copy()
+        mainReplacements.update({'instruments' : instruments})
+        self.createTemplate('main', 'main.ly', mainReplacements).run()
+
     def createTemplate(self, template, out, replacements):
         pipe = TemplatingPipe(
             TemplateFileReader(template),
             TemplateFileWriter(out)
         )
         pipe.setFillers([
-            VersionFiller(),
+            self.versionFiller,
             LoopingReplacementFiller(replacements),
-            CleanupFiller(),
+            self.cleanupFiller,
         ])
         return pipe
