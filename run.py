980319f8411d908fc3647e46d49e588103bf7879
Roelof Ruis <roelof@branchonline.nl>
2016-10-02 16:03:34 +0200

    Moved template object creation to factory

--- run.py
+++ run.py
@@ -1,27 +1,21 @@
-from templating.pipes import *
-from templating.io import *
-from templating.fillers import *
+from templating.factory import *
 
 # Testing the system
+factory = LilypondFactory()
 
-pipe = TemplatingPipe(TemplateFileReader('main'), TemplateFileWriter('out.ly'))
-pipe.setFillers([
-    LoopingReplacementFiller({
-        'version' : '2.18.2',
-        'title' : 'Title',
-        'subtitle' : 'Subtitle',
-        'composer' : 'Roelof Ruis',
-        'meter' : '',
-        'piece' : '',
-        'instruments' : [
-            {
-                'instrumentName' : 'violin',
-            },
-            {
-                'instrumentName' : 'guitar',
-            }
-        ]
-    }),
-    CleanupFiller(),
-])
-pipe.run()
+factory.createTemplate('main', 'out.ly', {
+    'version' : '2.18.2',
+    'title' : 'Title',
+    'subtitle' : 'Subtitle',
+    'composer' : 'Roelof Ruis',
+    'meter' : '',
+    'piece' : '',
+    'instruments' : [
+        {
+            'instrumentName' : 'violin',
+        },
+        {
+            'instrumentName' : 'guitar',
+        }
+    ]
+}).run()
---
+++ templating/factory.py
@@ -0,0 +1,15 @@
+from pipes import *
+from io import *
+from fillers import *
+
+class LilypondFactory():
+    def createTemplate(self, template, out, replacements):
+        pipe = TemplatingPipe(
+            TemplateFileReader(template),
+            TemplateFileWriter(out)
+        )
+        pipe.setFillers([
+            LoopingReplacementFiller(replacements),
+            CleanupFiller(),
+        ])
+        return pipe
