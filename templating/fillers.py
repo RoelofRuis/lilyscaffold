c958ba6567404700f6cf562e4681e2b846fa0211
Roelof Ruis <roelof@branchonline.nl>
2016-10-02 14:53:23 +0200

    fill to superclass + regex edit

--- templating/fillers.py
+++ templating/fillers.py
@@ -4,21 +4,21 @@ import re
 # Defines a basic filler
 class Filler():
     def fill(self, lines):
-        raise NotImplementedError('Fillers should implement fill')
+        linesOut = []
+        for line in lines:
+            linesOut.append(self.processLine(line))
+        return linesOut
+
+    def processLine(self, line):
+        raise NotImplementedError('Fillers should implement process line.')
 
 # Replaces simple statements from a list of replacements.
 class SimpleReplacementFiller(Filler):
     def __init__(self, replacements = {}):
         self.replacements = replacements
-
-    def fill(self, lines):
-        linesOut = []
-        for line in lines:
-            linesOut.append(self.processLine(line))
-        return linesOut
             
     def processLine(self, line):
-        matches = re.findall(r'@(.+?)!', line)
+        matches = re.findall(r'@([a-z_]+?)!', line)
         for match in matches:
             replacement = self.findReplacement(match)
             if replacement:
@@ -33,11 +33,5 @@ class SimpleReplacementFiller(Filler):
             
 # Cleans up simple statements
 class CleanupFiller(Filler):
-    def fill(self, lines):
-        linesOut = []
-        for line in lines:
-            linesOut.append(self.processLine(line))
-        return linesOut
-      
     def processLine(self, line):
-        return re.sub(r'@(.+?)!', '', line)
+        return re.sub(r'@([a-z_]+?)!', '', line)
