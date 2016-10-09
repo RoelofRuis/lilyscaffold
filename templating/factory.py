2514f57984e7b0fe7d50932ef981d80e28a2e0ec
Roelof Ruis <roelof@branchonline.nl>
2016-10-10 01:38:28 +0200

    Updated factory class so it can build a project

--- templating/factory.py
+++ templating/factory.py
@@ -1,22 +1,29 @@
 from pipes import *
 from io import *
 from fillers import *
+from os.path import join
 
 class LilypondFactory():
-    def createProject(self, general, options, instruments):
+    def __init__(self, projectName):
+        self.projectName = projectName
         self.versionFiller = VersionFiller()
         self.cleanupFiller = CleanupFiller()
-        self.createTemplate('options', 'options.ly', options).run()
-        for instrument in instruments:
-            self.createTemplate('instrument', instrument['instrumentName'] + '.ly', instrument).run()
-        mainReplacements = general.copy()
-        mainReplacements.update({'instruments' : instruments})
-        self.createTemplate('main', 'main.ly', mainReplacements).run()
 
-    def createTemplate(self, template, out, replacements):
+    def buildProject(self):
+        configReader = ProjectConfigReader(self.projectName)
+        config       = configReader.getConfig()
+        sourceFolder = join(self.projectName, config['meta']['sourceFolder'])
+        self.createTemplate('options', join(sourceFolder, 'options.ly'), config['options']).run()
+        for instrument in config['instruments']:
+            self.createTemplate('instrument', join(sourceFolder, instrument['instrumentName'] + '.ly'), instrument).run()
+        mainReplacements = config['general'].copy()
+        mainReplacements.update({'instruments' : config['instruments']})
+        self.createTemplate('main', join(sourceFolder, 'main.ly'), mainReplacements).run()
+
+    def createTemplate(self, template, outPath, replacements):
         pipe = TemplatingPipe(
             TemplateFileReader(template),
-            TemplateFileWriter(out)
+            TemplateFileWriter(outPath)
         )
         pipe.setFillers([
             self.versionFiller,
