cf40379409894abe0c83e581829667f9afa0d382
Roelof Ruis <roelof@branchonline.nl>
2016-10-16 23:20:28 +0200

    Fixing error getting wrong global variable

--- templating/io.py
+++ templating/io.py
@@ -61,8 +61,8 @@ class ProjectFileWriter(ProjectWriter):
         projectFolder = join(GLOBAL_CONFIG.get('projectFolder', 'out\\'), self.projectName)
         self._setupFolderIfNotExists(projectFolder)
         self._setupFolderIfNotExists(join(projectFolder, GLOBAL_CONFIG.get('metaFolderName', 'meta')))
-        self._setupFolderIfNotExists(join(projectFolder, GLOBAL_CONFIG.get('metaFolderName', 'src')))
-        self._setupFolderIfNotExists(join(projectFolder, GLOBAL_CONFIG.get('metaFolderName', 'out')))
+        self._setupFolderIfNotExists(join(projectFolder, GLOBAL_CONFIG.get('sourceFolderName', 'src')))
+        self._setupFolderIfNotExists(join(projectFolder, GLOBAL_CONFIG.get('outputFolderName', 'out')))
 
     def setupTemplate(self, templateName):
         src = join(
